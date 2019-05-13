b77114affc11f354db24cdf2d8cacb10f58266a9
mazdak <nazemi.works@gmail.com>
2019-05-13 09:38:24 -0700

    approval card with semantic-ui

---
+++ jsx/src/CommentDetail.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+const CommentDetail = (props) => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src={props.avatar} alt="avatar"/>
+      </a>
+
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+        </a>
+        <div className="metadata">
+          <span className="data">
+            {props.timeAgo}
+          </span>
+        </div>
+        <div className="text">
+          Nice blog post
+        </div>
+      </div>
+    </div>
+  );
+  
+}
+
+export default CommentDetail; 
--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => {
-  return ( 
-    <div> hio there </div>
-  )
-}
-ReactDOM.render( < App /> , document.querySelector('#root'));
+import faker from "faker";
+
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {   
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="sam"
+          timeAgo="today"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="sam"
+          timeAgo="today"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="sam"
+          timeAgo="today"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="sam"
+          timeAgo="today"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.querySelector("#root"));
